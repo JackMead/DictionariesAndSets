4b46d2bc67011b00d9ebdb53531f0712a1cf1b27
JackMead <Jack.Mead@softwire.com>
2018-01-26 17:29:24 +0000

    Basic working? version Currently thinks it recognises all strings...

---
+++ BloomFilters/BloomFilters/BloomFilter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace BloomFilters
+{
+    class BloomFilter
+    {
+        public bool[] BitMap { get; set; }
+
+        public BloomFilter()
+        {
+            BitMap = new bool[short.MaxValue-short.MinValue+1];
+        }
+
+        public void SetBits(string word)
+        {
+            var inputBytes = Encoding.ASCII.GetBytes(word);
+            var initialHashBytes = Hash(inputBytes);
+            
+            for (int i = 0; i < 8; i++)
+            { 
+                var newHashBytes = Hash(Hash(initialHashBytes));
+                var num = BitConverter.ToInt16(newHashBytes,i*2)-short.MinValue;
+                BitMap[num] = true;
+            }
+        }
+
+        public bool WasSet(string word)
+        {
+            var inputBytes = Encoding.ASCII.GetBytes(word);
+            var initialHashBytes = Hash(inputBytes);
+
+            for (int i = 0; i < 8; i++)
+            {
+                var newHashBytes = Hash(Hash(initialHashBytes));
+                var num = BitConverter.ToInt16(newHashBytes, i * 2) - short.MinValue;
+                if (!BitMap[num])
+                {
+                    return false;
+                } 
+            }
+            return true;
+        }
+
+        private byte[] Hash(byte[] inputBytes)
+        {
+            using (var md5 = MD5.Create())
+            {
+                return md5.ComputeHash(inputBytes);
+            }
+        }
+
+    }
+}
--- BloomFilters/BloomFilters/Program.cs
+++ BloomFilters/BloomFilters/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,80 @@ namespace BloomFilters
     {
         static void Main(string[] args)
         {
+            var p = new Program();
+            p.Run();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Loading words");
+            var words = LoadWords();
+            var bloomFilter = new BloomFilter();
+            Console.WriteLine("Preparing BloomFilter");
+
+            foreach (var word in words)
+            {
+                bloomFilter.SetBits(word);
+            }
+
+            Console.WriteLine("Checking if random strings are present in the dictionary:");
+            var recognised = 0;
+            var actual = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                if (i % 1000 == 0)
+                {
+                    Console.WriteLine("{0}/10000",i);
+                }
+                var testWord = Guid.NewGuid().ToString().Substring(0, 5);
+
+                if (bloomFilter.WasSet(testWord))
+                {
+                    recognised++;
+                    if (words.Contains(testWord))
+                    {
+                        actual++;
+                        Console.WriteLine(testWord);
+                    }
+                }
+            }
+            Console.WriteLine("The bloom filter accepted {0} whilst actually {1} were present",recognised, actual);
+
+            Console.WriteLine("Checking actual words present");
+            var random = new Random();
+            var missed = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                if (i % 1000 == 0)
+                {
+                    Console.WriteLine("{0}/10000", i);
+                }
+                var testWord = words[random.Next(words.Length)];
+
+                if (!bloomFilter.WasSet(testWord))
+                {
+                    missed++;
+                    Console.WriteLine("The algorithm denied having seen {0}", testWord);
+                }
+            }
+
+            Console.WriteLine();
+
+        }
+
+        public string[] LoadWords()
+        {
+            var listOfWords = new List<string>();
+            var path = "Words.txt";
+            using (var sr = new StreamReader(path))
+            {
+                var line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    listOfWords.Add(line);
+                }
+            }
+            return listOfWords.ToArray();
         }
     }
 }
